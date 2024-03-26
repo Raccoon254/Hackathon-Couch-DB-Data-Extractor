edf0f7f7412b16108fdb1c433d293669ba77e9b0
raccoon254 <tomsteve187@gmail.com>
2024-03-26 14:36:33 +0300

    Ignore the files created and move them into the data folder

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const nano = require('nano')({
 });
 const fs = require('fs');
 const path = require('path');
+//create a new dir called data and add the files in it
+if (!fs.existsSync('data')) {
+  fs.mkdirSync('data');
+}
+
+//make the data dir the current working directory
+process.chdir('data');
+//use the directory to store the files
 
 const fetchAndStoreDbDocuments = async (dbName) => {
   const db = nano.use(dbName);
@@ -16,7 +24,7 @@ const fetchAndStoreDbDocuments = async (dbName) => {
   const docs = rows.map(row => row.doc);
 
   // Store documents in a file as JSON
-  const filePath = path.join(__dirname, `${dbName}.json`);
+  const filePath = path.join(process.cwd(), `${dbName}.json`);
   fs.writeFileSync(filePath, JSON.stringify(docs, null, 2));
   console.log(`Stored ${docs.length} documents from ${dbName} in ${filePath}`);
 };
