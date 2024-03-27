e99b75801115fcd495bd69e4aeccab1e83fa7940
kimaru355 <emmanuelkimaru355@gmail.com>
2024-03-27 14:38:52 +0300

    use express.json middleware

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({ origin: "http://localhost:5173" }));
-app.use(express.json);
+app.use(express.json());
 
 //create a new dir called data
 if (!fs.existsSync("data")) {
